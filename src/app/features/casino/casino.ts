c3b5a767edb85166a443bc02569e7391619cc256
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2026-03-10 20:42:47 -0500

    refactor: remove navbar component and update casino feature

--- src/app/features/casino/casino.ts
+++ src/app/features/casino/casino.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { Footer } from '../../shared/components/footer/footer';
-import { Navbar } from '../../shared/components/navbar/navbar';
 
 @Component({
     selector: 'app-casino',
-    imports: [Navbar, Footer],
+    imports: [Footer],
     templateUrl: './casino.html',
     styleUrl: './casino.css'
 })
